e11841c60686912798abb2d8b7769fc24ea0a5c2
Pedro Lucas <izielucas2016@gmail.com>
2025-07-22 12:13:16 -0300

    feat: created DATABASE DER and Script in SQL to execute

---
+++ script.sql
@@ -0,0 +1,55 @@
+DROP DATABASE IF EXISTS forcatotal;
+
+CREATE DATABASE forcatotal;
+
+USE forcatotal;
+
+CREATE TABLE equipamento (
+    id int auto_increment primary key,
+    nome VARCHAR(200) NOT NULL,
+    marca VARCHAR(200) NOT NULL,
+    modelo VARCHAR(200) NOT NULL,
+    ano_de_fabricacao YEAR NOT NULL,
+    numero_de_serie VARCHAR(250) NOT NULL,
+    capacidade_maxima INT NOT NULL,
+    localizacao VARCHAR(100) NOT NULL,
+    estado ENUM(
+        'novo',
+        'bom',
+        'gasto',
+        'quebrado'
+    )
+);
+
+CREATE TABLE usuarios (
+    id int auto_increment primary key,
+    nome VARCHAR(200) NOT NULL,
+    cpf VARCHAR(14) NOT NULL UNIQUE,
+    telefone INT(11) NOT NULL UNIQUE,
+    email VARCHAR(200) NOT NULL UNIQUE,
+    senha_hash VARCHAR(250) NOT NULL,
+    tipo ENUM('Membro', 'Admin'),
+    id_membro INT,
+    Foreign Key (id_membro) REFERENCES membros (id)
+);
+
+CREATE TABLE membros (
+    id int auto_increment primary key,
+    nome VARCHAR(200) NOT NULL,
+    cpf VARCHAR(14) NOT NULL UNIQUE,
+    data_de_nascimento DATE NOT NULL,
+    endereco VARCHAR(250) NOT NULL,
+    telefone INT(11) NOT NULL UNIQUE,
+    data_de_matricula DATE,
+    plano_de_contrato ENUM('PLUS', 'PRO', 'OMEGA')
+);
+
+CREATE TABLE reservas (
+    id int auto_increment primary key,
+    id_usuario int not null,
+    id_equipamento int not null,
+    data_reserva DATETIME not null,
+    data_termino DATETIME NOT NULL,
+    Foreign Key (id_usuario) REFERENCES usuarios (id),
+    Foreign Key (id_equipamento) REFERENCES equipamento (id)
+);
